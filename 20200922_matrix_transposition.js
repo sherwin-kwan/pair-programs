db12964de6f4807b4d3b6b43f13eb6b658fcb83b
Sherwin <sherwin.kwan92@gmail.com>
2020-09-22 17:00:34 -0700

    added the matrix program

---
+++ 20200922_matrix_transposition.js
@@ -0,0 +1,72 @@
+const participants = ['Ayushi Sharma', 'Charles Thompson', 'Sherwin Kwan'];
+console.log('This pair program was created collaboratively on 22 Sep 2020 by:')
+for (let participant of participants) {
+  console.log(participant);
+}
+console.log('');
+
+// The Plan:
+// 
+
+const transpose = function(matrix) {
+  // Put your solution here
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (i === 0) {
+        result[j] = [];
+      }
+      result[j].push(matrix[i][j]); // inserts the value of matrix[i][j] at result[j][i]
+    }
+    // result
+  }
+  return result;
+};
+
+
+
+
+// // [ [1, 1, 1, 1], [2, 2, 2, 2]]
+// 1 
+// 2
+// 3
+// 4
+
+// Do not edit this function.
+const printMatrix = (matrix) => {
+  for (const row of matrix) {
+    for (const el of row) {
+      process.stdout.write(el + " ");
+    }
+    process.stdout.write('\n');
+  }
+};
+
+printMatrix(transpose([
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4]
+]));
+
+
+// // expected output
+// // 1 1 1 1 
+// // 2 2 2 2 
+// // 3 3 3 3 
+// // 4 4 4 4
+
+// console.log('----')
+
+printMatrix(transpose([
+  [1, 2],
+  [3, 4],
+  [5, 6]
+]));
+
+// console.log('----')
+
+printMatrix(transpose([
+  [1, 2, 3, 4, 5, 6, 7]
+]));
